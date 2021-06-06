9681d5c0519567b7bf9fdbd32e0399ba31536d5f
kirthika2403 <79511897+kirthika2403@users.noreply.github.com>
2021-06-06 22:08:09 +0530

    Update donorDetail.sql

--- sql/donorDetail.sql
+++ sql/donorDetail.sql
@@ -1,6 +1,6 @@
 create table donor_detail(
 donor_blood varchar(5) not null,
 donor_name varchar(100) not null,
-donor_number varchar(12) primary key,
+donor_number varchar(12) not null,
 donor_age int not null,
 donor_place varchar(100) not null);
