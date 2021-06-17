7d50ab6f5f27e711da53162df4eecdb6a2e7d917
Kirthika V <kirthy2411@gmail.com>
2021-06-17 12:17:42 +0530

    changes in sql code

--- sql/donorDetail.sql
+++ sql/donorDetail.sql
@@ -1,6 +1,8 @@
 create table donor_detail(
+user_name varchar(100) not null unique,
 donor_blood varchar(5) not null,
 donor_name varchar(100) not null,
 donor_number varchar(12) not null,
 donor_age int not null,
-donor_place varchar(100) not null);
+donor_place varchar(100) not null,
+donor_status varchar(10));
--- sql/userCredentials.sql
+++ sql/userCredentials.sql
@@ -1,4 +1,4 @@
 create table user_credential(
-username varchar(100) not null,
+username varchar(100) not null unique,
 userpassword varchar(100) not null,
-usermobilenumber varchar(100) not null unique);
+usermobilenumber varchar(100) not null);
